d6b5f331ec50b0b5326022ed17c61b21920f2b20
Pranam Lashkari <plashkari628@gmail.com>
2019-06-17 13:02:49 +0530

    Integrated cartesian_representation with units (#11)
    closes #6

--- include/boost/astronomy/coordinate/cartesian_representation.hpp
+++ include/boost/astronomy/coordinate/cartesian_representation.hpp
@@ -3,132 +3,351 @@
 
 
 #include <tuple>
+#include <type_traits>
 
+#include <boost/static_assert.hpp>
 #include <boost/geometry/core/cs.hpp>
 #include <boost/geometry/geometries/point.hpp>
 #include <boost/geometry/algorithms/transform.hpp>
-#include <boost/geometry/algorithms/equals.hpp>
-#include <boost/static_assert.hpp>
+#include <boost/geometry/strategies/strategy_transform.hpp>
+#include <boost/units/quantity.hpp>
+#include <boost/units/get_dimension.hpp>
+#include <boost/units/systems/si/dimensionless.hpp>
 
 #include <boost/astronomy/detail/is_base_template_of.hpp>
 #include <boost/astronomy/coordinate/base_representation.hpp>
-#include <boost/astronomy/coordinate/cartesian_differential.hpp>
 
 
-namespace boost
+namespace boost { namespace astronomy { namespace coordinate {
+
+namespace bu = boost::units;
+namespace bg = boost::geometry;
+
+//!Represents the coordinate in cartesian representation
+//!Uses three components to represent a point/vector (x, y, z)
+template
+<
+    typename CoordinateType=double,
+    typename XQuantity = bu::quantity<bu::si::dimensionless, CoordinateType>,
+    typename YQuantity = bu::quantity<bu::si::dimensionless, CoordinateType>,
+    typename ZQuantity = bu::quantity<bu::si::dimensionless, CoordinateType>
+>
+struct cartesian_representation: base_representation<3, bg::cs::cartesian, CoordinateType>
 {
-    namespace astronomy
+    ///@cond INTERNAL
+    BOOST_STATIC_ASSERT_MSG(
+        ((std::is_same<typename bu::get_dimension<XQuantity>::type,
+        typename bu::get_dimension<YQuantity>::type>::value) &&
+        (std::is_same<typename bu::get_dimension<YQuantity>::type,
+        typename bu::get_dimension<ZQuantity>::type>::value)),
+        "All components must have same dimensions");
+    ///@endcond
+
+public:
+
+    typedef XQuantity quantity1;
+    typedef YQuantity quantity2;
+    typedef ZQuantity quantity3;
+    //typename cartesian_representation
+    //    <CoordinateType, XQuantity, YQuantity, ZQuantity> this_type;
+
+    //default constructoer no initialization
+    cartesian_representation(){}
+
+    //!Constructs object from provided quantities
+    //!Each quantity can have different units but with same datatypes
+    cartesian_representation(XQuantity const& x, YQuantity const& y, ZQuantity const& z)
     {
-        namespace coordinate
-        {
-            //!Represents the coordinate in cartesian representation
-            //!Uses three components to represent a point/vector (x, y, z)
-            struct cartesian_representation : public boost::astronomy::coordinate::base_representation
-                <3, boost::geometry::cs::cartesian>
-            {
-            public:
-                //default constructoer no initialization
-                cartesian_representation(){}
+        this->set_x_y_z(x, y, z);
+    }
 
-                //!constructs object from provided value of coordinates
-                cartesian_representation(double x, double y=0.0, double z=0.0)
-                {
-                    boost::geometry::set<0>(this->point, x);
-                    boost::geometry::set<1>(this->point, y);
-                    boost::geometry::set<2>(this->point, z);
-                }
+    //!Constructs object from boost::geometry::model::point object
+    //!Quantities have to be specified explicitly
+    template
+    <
+        std::size_t OtherDimensionCount,
+        typename OtherCoordinateSystem,
+        typename OtherCoordinateType
+    >
+    cartesian_representation
+    (
+        bg::model::point
+        <
+            OtherCoordinateType,
+            OtherDimensionCount,
+            OtherCoordinateSystem
+        > const& pointObject
+    )
+    {
+        bg::transform(pointObject, this->point);
 
-                //!constructs object from boost::geometry::model::point object
-                template <std::size_t DimensionCount, typename System>
-                cartesian_representation(boost::geometry::model::point<double, DimensionCount, System> const& pointObject)
-                {
-                    boost::geometry::transform(pointObject, this->point);
+    }
 
-                }
+    //!Copy constructor
+    cartesian_representation
+    (
+        cartesian_representation
+        <
+            CoordinateType,
+            XQuantity,
+            YQuantity,
+            ZQuantity
+        > const& object
+    )
+    {
+        this->point = object.get_point();
+    }
 
-                //copy constructor
-                cartesian_representation(cartesian_representation const& object)
-                {
-                    this->point = object.get_point();
-                }
+    //!Constructs object from any type of representation
+    //!Quantities have to be specified explicitly
+    template <typename Representation>
+    cartesian_representation(Representation const& other)
+    {
+        BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
+            <boost::astronomy::coordinate::base_representation, Representation>::value),
+            "No constructor found with given argument type");
 
-                //!constructs object from any type of representation
-                template <typename Representation>
-                cartesian_representation(Representation const& other)
-                {
-                    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
-                        <boost::astronomy::coordinate::base_representation, Representation>::value),
-                        "No constructor found with given argument type");
+        bg::transform(other.get_point(), this->point);
+    }
 
-                    boost::geometry::transform(other.get_point(), this->point);
-                }
+    //! Returns the (x, y, z) in the form of tuple
+    std::tuple<XQuantity, YQuantity, ZQuantity> get_x_y_z() const
+    {
+        return std::make_tuple(this->get_x(), this->get_y(), this->get_z());
+    }
 
-                //! returns the (x, y, z) in the form of tuple
-                std::tuple<double, double, double> get_xyz() const
-                {
-                    return std::make_tuple(boost::geometry::get<0>(this->point),
-                        boost::geometry::get<1>(this->point), boost::geometry::get<2>(this->point));
-                }
+    //!Returns the x quantity component of point
+    XQuantity get_x() const
+    {
+        return bg::get<0>(this->point)* typename XQuantity::unit_type();
+    }
 
-                //!returns the x component of point
-                double get_x() const
-                {
-                    return boost::geometry::get<0>(this->point);
-                }
+    //!Returns the y quantity component of point
+    YQuantity get_y() const
+    {
+        return bg::get<1>(this->point)* typename YQuantity::unit_type();
+    }
 
-                //!returns the y component of point
-                double get_y() const
-                {
-                    return boost::geometry::get<1>(this->point);
-                }
+    //!Returns the z quantity component of point
+    ZQuantity get_z() const
+    {
+        return bg::get<2>(this->point)* typename ZQuantity::unit_type();
+    }
 
-                //!returns the z component of point
-                double get_z() const
-                {
-                    return boost::geometry::get<2>(this->point);
-                }
+    //!Set value of (x, y, z) in current object
+    void set_x_y_z(XQuantity const& x, YQuantity const& y, ZQuantity const& z)
+    {
+        this->set_x(x);
+        this->set_y(y);
+        this->set_z(z);
+    }
 
-                //!set value of (x, y, z) in current object
-                void set_xyz(double x, double y, double z)
-                {
-                    boost::geometry::set<0>(this->point, x);
-                    boost::geometry::set<1>(this->point, y);
-                    boost::geometry::set<2>(this->point, z);
-                }
+    //!Set value of x quantity component of point
+    void set_x(XQuantity const& x)
+    {
+        bg::set<0>(this->point, x.value());
+    }
 
-                //!set value of x component of point
-                void set_x(double x)
-                {
-                    boost::geometry::set<0>(this->point, x);
-                }
+    //!Set value of y quantity component of point
+    void set_y(YQuantity const& y)
+    {
+        bg::set<1>(this->point, y.value());
+    }
 
-                //!set value of y component of point
-                void set_y(double y)
-                {
-                    boost::geometry::set<1>(this->point, y);
-                }
+    //!Set value of z quantity component of point
+    void set_z(ZQuantity const& z)
+    {
+        bg::set<2>(this->point, z.value());
+    }
 
-                //!set value of z component of point
-                void set_z(double z)
-                {
-                    boost::geometry::set<2>(this->point, z);
-                }
+    //!"+" operator to add any representation or differential
+    template
+    <
+        typename Addend
+    >
+    cartesian_representation
+    <
+        CoordinateType,
+        XQuantity,
+        YQuantity,
+        ZQuantity
+    >
+    operator +(Addend const& addend) const
+    {
+        bg::model::point
+        <
+            typename std::conditional
+            <
+                sizeof(typename Addend::type) >= sizeof(CoordinateType),
+                typename Addend::type,
+                CoordinateType
+            >::type,
+            3,
+            bg::cs::cartesian
+        > result;
 
-                boost::astronomy::coordinate::cartesian_representation 
-                    operator +(boost::astronomy::coordinate::cartesian_differential const& diff) const 
-                {
-                    boost::astronomy::coordinate::cartesian_representation temp(this->point);
+    auto cartesian = make_cartesian_representation(addend);
 
-                    temp.set_x(temp.get_x() + diff.get_dx());
-                    temp.set_y(temp.get_y() + diff.get_dy());
-                    temp.set_z(temp.get_z() + diff.get_dz());
+    //performing calculation to find the sum
+    bg::set<0>(result, (this->get_x().value() +
+        static_cast<quantity1>(cartesian.get_x()).value()));
+    bg::set<1>(result, (this->get_y().value() +
+        static_cast<quantity2>(cartesian.get_y()).value()));
+    bg::set<2>(result, (this->get_z().value() +
+        static_cast<quantity3>(cartesian.get_z()).value()));
 
-                    return temp;
-                }
+    return cartesian_representation
+        <
+            CoordinateType,
+            XQuantity,
+            YQuantity,
+            ZQuantity
+        >(result);
+    }
 
-            }; //cartesian_representation
-        } //namespace coordinate
-    } //namespace astronomy
-} //namespace boost
+}; //cartesian_representation
+
+
+//!Constructs object from provided quantities
+//!Each quantity can have different units but with same datatypes
+template
+<
+    typename CoordinateType,
+    template <typename Unit1, typename CoordinateType_> class XQuantity,
+    template <typename Unit2, typename CoordinateType_> class YQuantity,
+    template <typename Unit3, typename CoordinateType_> class ZQuantity,
+    typename Unit1,
+    typename Unit2,
+    typename Unit3
+>
+cartesian_representation
+<
+    CoordinateType,
+    XQuantity<Unit1, CoordinateType>,
+    YQuantity<Unit2, CoordinateType>,
+    ZQuantity<Unit3, CoordinateType>
+> make_cartesian_representation
+(
+    XQuantity<Unit1, CoordinateType> const& x,
+    YQuantity<Unit2, CoordinateType> const& y,
+    ZQuantity<Unit3, CoordinateType> const& z
+)
+{
+    return cartesian_representation
+        <
+            CoordinateType,
+            XQuantity<Unit1, CoordinateType>,
+            YQuantity<Unit2, CoordinateType>,
+            ZQuantity<Unit3, CoordinateType>
+        >(x, y, z);
+}
+
+
+//!Convert current quantities of cartesian_representation to new quantities. 
+template
+<
+    typename ReturnCoordinateType,
+    typename ReturnXQuantity,
+    typename ReturnYQuantity,
+    typename ReturnZQuantity,
+    typename CoordinateType,
+    typename XQuantity,
+    typename YQuantity,
+    typename ZQuantity
+>
+cartesian_representation
+<
+    ReturnCoordinateType,
+    ReturnXQuantity,
+    ReturnYQuantity,
+    ReturnZQuantity
+>
+make_cartesian_representation
+(cartesian_representation<CoordinateType, XQuantity, YQuantity, ZQuantity> const& other)
+{
+    return make_cartesian_representation(
+        static_cast<ReturnXQuantity>(other.get_x()),
+        static_cast<ReturnYQuantity>(other.get_y()),
+        static_cast<ReturnZQuantity>(other.get_z())
+    );
+}
+
+
+//!Create copy of cartesian_representation
+template
+<
+    typename CoordinateType,
+    typename XQuantity,
+    typename YQuantity,
+    typename ZQuantity
+>
+cartesian_representation<CoordinateType, XQuantity, YQuantity, ZQuantity>
+make_cartesian_representation
+(cartesian_representation<CoordinateType, XQuantity, YQuantity, ZQuantity> const& other)
+{
+    return cartesian_representation
+        <
+            CoordinateType,
+            XQuantity,
+            YQuantity,
+            ZQuantity
+        >(other);
+}
+
+//!Create cartesian_representation from boost::geometry::point
+//!Quantity types are to be specifed explicitly or dimensionless is taken by default
+template
+<
+    typename CoordinateType = double,
+    typename XQuantity = bu::quantity<bu::si::dimensionless, CoordinateType>,
+    typename YQuantity = bu::quantity<bu::si::dimensionless, CoordinateType>,
+    typename ZQuantity = bu::quantity<bu::si::dimensionless, CoordinateType>,
+    std::size_t OtherDimensionCount,
+    typename OtherCoordinateSystem,
+    typename OtherCoordinateType
+>
+cartesian_representation<CoordinateType, XQuantity, YQuantity, ZQuantity>
+make_cartesian_representation
+(
+    bg::model::point
+    <
+    OtherCoordinateType,
+    OtherDimensionCount,
+    OtherCoordinateSystem
+    > const& pointObject)
+{
+    return cartesian_representation
+        <
+            CoordinateType,
+            XQuantity,
+            YQuantity,
+            ZQuantity
+        >(pointObject);
+}
+
+
+//!Create cartesian_representation from other type of representations
+template <typename OtherRepresentation>
+cartesian_representation
+<
+    typename OtherRepresentation::type,
+    typename OtherRepresentation::quantity3,
+    typename OtherRepresentation::quantity3,
+    typename OtherRepresentation::quantity3
+>
+make_cartesian_representation(OtherRepresentation const& other)
+{
+    bg::model::point<typename OtherRepresentation::type, 3, bg::cs::cartesian> result;
+    bg::transform(other.get_point(), result);
+
+    return cartesian_representation
+        <
+        typename OtherRepresentation::type,
+        typename OtherRepresentation::quantity3,
+        typename OtherRepresentation::quantity3,
+        typename OtherRepresentation::quantity3
+        >(result);
+}
+}}} //namespace boost::astronomy::coordinate
 
 #endif // !BOOST_ASTRONOMY_COORDINATE_CARTESIAN_REPRESENTATION_HPP
--- test/representation.cpp
+++ test/representation.cpp
@@ -2,136 +2,161 @@
 
 
 #include <boost/test/unit_test.hpp>
-#include <boost/astronomy/coordinate/representation.hpp>
+#include <boost/units/quantity.hpp>
+#include <boost/units/systems/si/prefixes.hpp>
+#include <boost/units/systems/si/length.hpp>
+#include <boost/geometry/core/cs.hpp>
+#include <boost/geometry/geometries/point.hpp>
+
+#include <boost/astronomy/coordinate/cartesian_representation.hpp>
+#include <boost/astronomy/coordinate/arithmetic.hpp>
+
 
 using namespace std;
 using namespace boost::astronomy::coordinate;
+using namespace boost::units::si;
+using namespace boost::geometry;
+using namespace boost::units;
+
 
 BOOST_AUTO_TEST_SUITE(representation_constructor)
 
 BOOST_AUTO_TEST_CASE(cartesian)
 {
     //checking construction from value
-    cartesian_representation point1(1.5, 9.0, 3.5);
-    BOOST_CHECK_CLOSE(point1.get_x(), 1.5, 0.001);
-    BOOST_CHECK_CLOSE(point1.get_y(), 9.0, 0.001);
-    BOOST_CHECK_CLOSE(point1.get_z(), 3.5, 0.001);
+    auto point1 = make_cartesian_representation
+    (1.5*meter, 9.0*kilo*meter, 3.0*centi*meter);
+    BOOST_CHECK_CLOSE(point1.get_x().value(), 1.5, 0.001);
+    BOOST_CHECK_CLOSE(point1.get_y().value(), 9.0, 0.001);
+    BOOST_CHECK_CLOSE(point1.get_z().value(), 3, 0.001);
 
     //copy constructor
-    cartesian_representation point2(point1);
-    BOOST_CHECK_CLOSE(point1.get_x(), point2.get_x(), 0.001);
-    BOOST_CHECK_CLOSE(point1.get_y(), point2.get_y(), 0.001);
-    BOOST_CHECK_CLOSE(point1.get_z(), point2.get_z(), 0.001);
+    auto point2 = make_cartesian_representation(point1);
+    BOOST_CHECK_CLOSE(point1.get_x().value(), point2.get_x().value(), 0.001);
+    BOOST_CHECK_CLOSE(point1.get_y().value(), point2.get_y().value(), 0.001);
+    BOOST_CHECK_CLOSE(point1.get_z().value(), point2.get_z().value(), 0.001);
 
     //constructing from boost::geometry::model::point
-    boost::geometry::model::point<double, 2, boost::geometry::cs::spherical<boost::geometry::degree>> model_point(30, 60);
-    cartesian_representation point3(model_point);
-    BOOST_CHECK_CLOSE(point3.get_x(), 0.75, 0.001);
-    BOOST_CHECK_CLOSE(point3.get_y(), 0.4330127019, 0.001);
-    BOOST_CHECK_CLOSE(point3.get_z(), 0.5, 0.001);
+    model::point<double, 2, cs::spherical<degree>> model_point(30, 60);
+    auto point3 = make_cartesian_representation
+        <double, quantity<length>, quantity<length>, quantity<length>>(model_point);
+    BOOST_CHECK_CLOSE(point3.get_x().value(), 0.75, 0.001);
+    BOOST_CHECK_CLOSE(point3.get_y().value(), 0.4330127019, 0.001);
+    BOOST_CHECK_CLOSE(point3.get_z().value(), 0.5, 0.001);
 
-    //constructing from another representation
-    spherical_representation<radian> spherical_point(0.523599, 1.047198, 1);
-    cartesian_representation point4(spherical_point);
-    BOOST_CHECK_CLOSE(point4.get_x(), 0.75, 0.001);
-    BOOST_CHECK_CLOSE(point4.get_y(), 0.4330127019, 0.001);
-    BOOST_CHECK_CLOSE(point4.get_z(), 0.5, 0.001);
+    //Conversion from one unit type to other
+    auto point4 = make_cartesian_representation
+        <double, quantity<length>, quantity<length>, quantity<length>>(point1);
+    BOOST_CHECK_CLOSE(point4.get_x().value(), 1.5, 0.001);
+    BOOST_CHECK_CLOSE(point4.get_y().value(), 9000.0, 0.001);
+    BOOST_CHECK_CLOSE(point4.get_z().value(), 0.03, 0.001);
+
+    ////constructing from another representation
+    //spherical_representation<radian> spherical_point(0.523599, 1.047198, 1);
+    //cartesian_representation point4(spherical_point);
+    //BOOST_CHECK_CLOSE(point4.get_x(), 0.75, 0.001);
+    //BOOST_CHECK_CLOSE(point4.get_y(), 0.4330127019, 0.001);
+    //BOOST_CHECK_CLOSE(point4.get_z(), 0.5, 0.001);
 }
 
-BOOST_AUTO_TEST_CASE(spherical)
-{
-    //checking construction from value
-    spherical_representation<degree> point1(45.0, 18, 3.5);
-    BOOST_CHECK_CLOSE(point1.get_lat(), 45.0, 0.001);
-    BOOST_CHECK_CLOSE(point1.get_lon(), 18.0, 0.001);
-    BOOST_CHECK_CLOSE(point1.get_dist(), 3.5, 0.001);
-
-    //copy constructor
-    spherical_representation<degree> point2(point1);
-    BOOST_CHECK_CLOSE(point1.get_lat(), point2.get_lat(), 0.001);
-    BOOST_CHECK_CLOSE(point1.get_lon(), point2.get_lon(), 0.001);
-    BOOST_CHECK_CLOSE(point1.get_dist(), point2.get_dist(), 0.001);
-
-    //constructing from boost::geometry::model::point
-    boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian> model_point(50, 20, 30);
-    spherical_representation<radian> point3(model_point);
-    BOOST_CHECK_CLOSE(point3.get_lat(), 0.38050637711237, 0.001);
-    BOOST_CHECK_CLOSE(point3.get_lon(), 1.0625290806236, 0.001);
-    BOOST_CHECK_CLOSE(point3.get_dist(), 61.64414002969, 0.001);
-
-    //constructing from another representation
-    cartesian_representation cartesian_point(60, 45, 85);
-    spherical_representation<degree> point4(cartesian_point);
-    BOOST_CHECK_CLOSE(point4.get_lat(), 36.869897645844, 0.001);
-    BOOST_CHECK_CLOSE(point4.get_lon(), 41.423665625003, 0.001);
-    BOOST_CHECK_CLOSE(point4.get_dist(), 113.35784048755, 0.001);
-}
+//BOOST_AUTO_TEST_CASE(spherical)
+//{
+//    //checking construction from value
+//    spherical_representation<degree> point1(45.0, 18, 3.5);
+//    BOOST_CHECK_CLOSE(point1.get_lat(), 45.0, 0.001);
+//    BOOST_CHECK_CLOSE(point1.get_lon(), 18.0, 0.001);
+//    BOOST_CHECK_CLOSE(point1.get_dist(), 3.5, 0.001);
+//
+//    //copy constructor
+//    spherical_representation<degree> point2(point1);
+//    BOOST_CHECK_CLOSE(point1.get_lat(), point2.get_lat(), 0.001);
+//    BOOST_CHECK_CLOSE(point1.get_lon(), point2.get_lon(), 0.001);
+//    BOOST_CHECK_CLOSE(point1.get_dist(), point2.get_dist(), 0.001);
+//
+//    //constructing from boost::geometry::model::point
+//    boost::geometry::model::point<double, 3, boost::geometry::cs::cartesian> model_point(50, 20, 30);
+//    spherical_representation<radian> point3(model_point);
+//    BOOST_CHECK_CLOSE(point3.get_lat(), 0.38050637711237, 0.001);
+//    BOOST_CHECK_CLOSE(point3.get_lon(), 1.0625290806236, 0.001);
+//    BOOST_CHECK_CLOSE(point3.get_dist(), 61.64414002969, 0.001);
+//
+//    //constructing from another representation
+//    cartesian_representation cartesian_point(60, 45, 85);
+//    spherical_representation<degree> point4(cartesian_point);
+//    BOOST_CHECK_CLOSE(point4.get_lat(), 36.869897645844, 0.001);
+//    BOOST_CHECK_CLOSE(point4.get_lon(), 41.423665625003, 0.001);
+//    BOOST_CHECK_CLOSE(point4.get_dist(), 113.35784048755, 0.001);
+//}
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(representation_functions)
 
 BOOST_AUTO_TEST_CASE(cross_product)
 {
-    cartesian_representation point1(15, 25, 30);
-    spherical_representation<degree> point2(45, 45, 3);
+    auto point1 = make_cartesian_representation(3.0 * meters, 5.0 * kilo *meters, 4.0 * mega * meters);
+    auto point2 = make_cartesian_representation(3.0 * milli * meters, 5.0 * centi * meters, 4.0 * meters);
+    //spherical_representation<degree> point2(45, 45, 3);
 
-    auto result = point1.cross<cartesian_representation>(point2);
+    auto result = cross(point1, point2);
 
-    BOOST_CHECK_CLOSE(result.get_x(), 8.0330086, 0.001);
-    BOOST_CHECK_CLOSE(result.get_y(), 13.18019484, 0.001);
-    BOOST_CHECK_CLOSE(result.get_z(), -15, 0.001);
+    BOOST_CHECK_CLOSE(result.get_x().value(), -180.0, 0.001);
+    BOOST_CHECK_CLOSE(result.get_y().value(), 11.988, 0.001);
+    BOOST_CHECK_CLOSE(result.get_z().value(), -1485.0, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(dot_product)
 {
-    spherical_representation<degree> point1(30, 60, 3), point2(60, 30);
+    auto point1 = make_cartesian_representation(3.0 * meters, 5.0 * kilo *meters, 4.0 * mega * meters);
+    auto point2 = make_cartesian_representation(3.0 * milli * meters, 5.0 * centi * meters, 4.0 * meters);
 
-    double result = point1.dot(point2);
+    auto result = dot(point1, point2);
 
-    BOOST_CHECK_CLOSE(result, 2.4240381058501, 0.001);
+    BOOST_CHECK_CLOSE(result.value(), 16000250009.0, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(unit_vector)
 {
-    cartesian_representation point1(25, 36, 90);
+    auto point1 = make_cartesian_representation(25.0*meter, 36.0*meter, 90.0*meter);
 
-    auto result = point1.unit_vector<cartesian_representation>();
+    auto result = boost::astronomy::coordinate::unit_vector(point1);
 
-    BOOST_CHECK_CLOSE(result.get_x(), 0.2497379127153113, 0.001);
-    BOOST_CHECK_CLOSE(result.get_y(), 0.3596225943100483, 0.001);
-    BOOST_CHECK_CLOSE(result.get_z(), 0.8990564857751207, 0.001);
-
+    BOOST_CHECK_CLOSE(result.get_x().value(), 0.2497379127153113, 0.001);
+    BOOST_CHECK_CLOSE(result.get_y().value(), 0.3596225943100483, 0.001);
+    BOOST_CHECK_CLOSE(result.get_z().value(), 0.8990564857751207, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(magnitude)
 {
-    cartesian_representation point1(25, 36, 90);
+    auto point1 = make_cartesian_representation(25.0 * meter, 3600.0 * centi*meter, 90.0 * meter);
 
-    double result = point1.magnitude();
+    auto result = boost::astronomy::coordinate::magnitude(point1);
 
-    BOOST_CHECK_CLOSE(result, 100.1049449328054, 0.001);
+    BOOST_CHECK_CLOSE(result.value(), 100.1049449328054, 0.001);
 
 }
 
 BOOST_AUTO_TEST_CASE(sum)
 {
-    cartesian_representation point1(10, 20, 30), point2(50, 60, 30);
+    auto point1 = make_cartesian_representation(10.0 * meter, 20.0 * kilo * meters, 30.0 * meter);
+    auto point2 = make_cartesian_representation(50.0 * centi * meter, 60.0 * meter, 30.0 * meter);
 
-    auto result = point1.sum<spherical_representation<degree>>(point2);
+    auto result = point1 + point2;
 
-    BOOST_CHECK_CLOSE(result.get_lat(), 53.130102354156, 0.001);
-    BOOST_CHECK_CLOSE(result.get_lon(), 59.036243467927, 0.001);
-    BOOST_CHECK_CLOSE(result.get_dist(), 116.61903789691, 0.001);
+    BOOST_CHECK_CLOSE(result.get_x().value(), 10.5, 0.001);
+    BOOST_CHECK_CLOSE(result.get_y().value(), 20.06, 0.001);
+    BOOST_CHECK_CLOSE(result.get_z().value(), 60, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(mean)
 {
-    cartesian_representation point1(10, 20, 30), point2(50, 60, 30);
-    auto result = point1.mean<cartesian_representation>(point2);
+    auto point1 = make_cartesian_representation(10.0 * meter, 20.0 * kilo * meters, 30.0 * meter);
+    auto point2 = make_cartesian_representation(50.0 * centi * meter, 60.0 * meter, 30.0 * meter);
 
-    BOOST_CHECK_CLOSE(result.get_x(), 30.0, 0.001);
-    BOOST_CHECK_CLOSE(result.get_y(), 40.0, 0.001);
-    BOOST_CHECK_CLOSE(result.get_z(), 30.0, 0.001);
+    auto result = boost::astronomy::coordinate::mean(point1, point2);
+
+    BOOST_CHECK_CLOSE(result.get_x().value(), 5.25, 0.001);
+    BOOST_CHECK_CLOSE(result.get_y().value(), 10.03, 0.001);
+    BOOST_CHECK_CLOSE(result.get_z().value(), 30.0, 0.001);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
