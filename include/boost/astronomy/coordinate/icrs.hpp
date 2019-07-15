0d96499a31a2b19f223e294308eed892b0ea0032
Sarthak Singhal <singhalsarthak2007@gmail.com>
2019-07-15 11:59:23 +0530

    Integrated icrs.hpp with units

--- include/boost/astronomy/coordinate/icrs.hpp
+++ include/boost/astronomy/coordinate/icrs.hpp
@@ -1,51 +1,57 @@
 #ifndef BOOST_ASTRONOMY_COORDINATE_ICRS_HPP
 #define BOOST_ASTRONOMY_COORDINATE_ICRS_HPP
 
-#include <string>
 #include <boost/astronomy/coordinate/base_equatorial_frame.hpp>
 
-namespace boost
+namespace boost { namespace astronomy { namespace coordinate {
+
+template
+<
+    typename Representation, typename Differential
+>
+struct icrs : public base_equatorial_frame<Representation, Differential>
 {
-    namespace astronomy
-    {
-        namespace coordinate
-        {
-            template <typename RepresentationDegreeOrRadian = boost::astronomy::coordinate::degree,
-                typename DifferentialDegreeOrRadian = boost::astronomy::coordinate::degree>
-            struct icrs : public boost::astronomy::coordinate::base_equatorial_frame
-                <RepresentationDegreeOrRadian, DifferentialDegreeOrRadian>
-            {
-            public:
-                //default constructor no initialization
-                icrs() {}
 
-                template <typename Representation>
-                icrs(Representation const& representation_data) : base_equatorial_frame
-                    <RepresentationDegreeOrRadian, DifferentialDegreeOrRadian>(representation_data) {}
+public:
+    //default constructor no initialization
+    icrs() {}
 
-                /*!RA is expected to be a double if value is in degree/radian
-                if want to provide value in hours minute sec formate then a string expected with particular format
-                hour angle formate: "hhmmss..."
-                first two char as hours next two as minutes and remaining are treated as seconds
-                hour angles are converted to degree/radian and then stored
-                eg: if RA = 06h 45m 08.9s then value should be provided like "064508.9" */
-                template <typename RaType>
-                icrs(double dec, RaType const& ra, double distance) : base_equatorial_frame
-                    <RepresentationDegreeOrRadian, DifferentialDegreeOrRadian>(dec, ra, distance) {}
+    //!constructs object from another representation object
+    template <typename OtherRepresentation>
+    icrs(OtherRepresentation const& representation_data) : base_equatorial_frame
+        <Representation, Differential>(representation_data) {}
 
-                template <typename RaType>
-                icrs(double dec, RaType const& ra, double distance, double pm_dec, double pm_ra_cosdec, double radial_velocity) :
-                    base_equatorial_frame<RepresentationDegreeOrRadian, DifferentialDegreeOrRadian>
-                    (dec, ra, distance, pm_dec, pm_ra_cosdec, radial_velocity) {}
+    //!constructs object from provided components of representation
+    icrs
+    (
+        typename Representation::quantity1 const& dec,
+        typename Representation::quantity2 const& ra,
+        typename Representation::quantity3 const& distance
+    ) : base_equatorial_frame<Representation, Differential>(dec, ra, distance) {}
 
-                template <typename Representation, typename Differential>
-                icrs(Representation const& representation_data, Differential const& diff) :
-                    base_equatorial_frame<RepresentationDegreeOrRadian, DifferentialDegreeOrRadian>
-                    (representation_data, diff) {}
-            };
-        } //namespace coordinate
-    } //namespace astronomy
-} //namespace boost
+    //!constructs object from provided components of representation and differential
+    icrs
+    (
+        typename Representation::quantity1 const& dec,
+        typename Representation::quantity2 const& ra,
+        typename Representation::quantity3 const& distance,
+        typename Differential::quantity1 const& pm_dec,
+        typename Differential::quantity2 const& pm_ra_cosdec,
+        typename Differential::quantity3 const& radial_velocity
+    ) : base_equatorial_frame<Representation, Differential>
+            (dec, ra, distance, pm_dec, pm_ra_cosdec, radial_velocity) {}
+
+    //!constructs object from other representation and differential objects
+    template <typename OtherRepresentation, typename OtherDifferential>
+    icrs
+    (
+        OtherRepresentation const& representation_data,
+        OtherDifferential const& differential_data
+    ) : base_equatorial_frame<Representation, Differential>
+            (representation_data, differential_data) {}
+};
+
+}}} //namespace boost::astronomy::coordinate
 
 #endif // !BOOST_ASTRONOMY_COORDINATE_ICRS_HPP
 
