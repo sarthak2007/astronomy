dec258a168a3128afe5e28030e4f46677fc3daa0
Pranam Lashkari <plashkari628@gmail.com>
2019-07-23 00:32:31 +0530

    `base_frame` integrated with units (#37)
    closes #36

--- include/boost/astronomy/coordinate/base_frame.hpp
+++ include/boost/astronomy/coordinate/base_frame.hpp
@@ -13,89 +13,96 @@
 #include <boost/astronomy/coordinate/representation.hpp>
 #include <boost/astronomy/coordinate/differential.hpp>
 #include <boost/astronomy/detail/is_base_template_of.hpp>
+#include <boost/astronomy/coordinate/arithmetic.hpp>
 
 
-namespace boost 
+namespace boost { namespace astronomy { namespace coordinate {
+
+typedef boost::geometry::degree degree;
+typedef boost::geometry::radian radian;
+
+namespace bu = boost::units;
+namespace bg = boost::geometry;
+
+template <typename Representation, typename Differential>
+struct base_frame
 {
-    namespace astronomy
+    ///@cond INTERNAL
+    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
+        <boost::astronomy::coordinate::base_representation, Representation>::value),
+        "First template argument is expected to be a representation class");
+    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
+        <boost::astronomy::coordinate::base_differential, Differential>::value),
+        "Second template argument is expected to be a differential class");
+    ///@endcond
+
+protected:
+    Representation data;
+    Differential motion;
+
+public:
+    typedef Representation representation;
+    typedef Differential differential;
+
+    //!returns 2d propermotion
+    std::tuple
+    <
+        typename Differential::quantity1,
+        typename Differential::quantity2
+    > get_proper_motion() const
     {
-        namespace coordinate
-        {
-            typedef boost::geometry::degree degree;
-            typedef boost::geometry::radian radian;
+        return std::make_tuple
+        (
+            Differential::quantity1::from_value(bg::get<0>(motion.get_differential())),
+            Differential::quantity2::from_value(bg::get<1>(motion.get_differential())),
+        );
+    }
 
-            template <typename Representation, typename Differential>
-            struct base_frame
-            {
-            protected:
-                Representation data;
-                Differential motion;
+    //!returns radial velocity (line of sight velocity) of the object
+    typename Differential::quantity3 get_radial_velocity() const
+    {
+        return Differential::quantity3::from_value(bg::get<2>(motion.get_differential()));
+    }
 
-            public:
+    /*!returns differential data in form of boost::geometry::model::point 
+    having components (pm_lat, pm_lon, radial_velocity) including cos(lat) component*/
+    Differential get_differential() const
+    {
+        return motion;
+    }
 
-                //!returns 2d propermotion
-                std::tuple<double, double> proper_motion() const
-                {
-                    return std::make_tuple(boost::geometry::get<0>(data.get_differential()), 
-                        boost::geometry::get<1>(data.get_differential()));
-                }
+    //!set differential for the motion of the object
+    void set_differential(Differential const& other)
+    {
+        this->motion = other;
+    }
 
-                //!returns radial velocity (line of sight velocity) of the object
-                double radial_velocity() const
-                {
-                    return boost::geometry::get<2>(data.get_differential());
-                }
+    /*!returns coordinate data in form of boost::geometry::model::point
+    having components (lat, lon, distance)*/
+    Representation get_data() const
+    {
+        return data;
+    }
 
-                /*!returns differential data in form of boost::geometry::model::point 
-                having components (pm_lat, pm_lon, radial_velocity) including cos(lat) component*/
-                Differential get_differential() const
-                {
-                    return motion;
-                }
+    //!returns data in specified subclass of base_representation
+    //template <typename ReturnType>
+    //ReturnType represent_as() const
+    //{
+    //    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
+    //        <boost::astronomy::coordinate::base_representation, ReturnType>::value), 
+    //        "return type is expected to be a representation class");
 
-                //!set differential for the motion of the object
-                template <typename OtherRepresentation>
-                void set_differential(OtherRepresentation const& other)
-                {
-                    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
-                        <boost::astronomy::coordinate::base_differential, OtherRepresentation>::value),
-                        "argument type is expected to be a differential class");
+    //    return ReturnType(data);
+    //}
 
-                    this->motion = other;
-                }
-
-                /*!returns coordinate data in form of boost::geometry::model::point
-                having components (lat, lon, distance)*/
-                Representation get_data() const
-                {
-                    return data;
-                }
-
-                //!returns data in specified subclass of base_representation
-                template <typename ReturnType>
-                ReturnType represen_as() const
-                {
-                    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
-                        <boost::astronomy::coordinate::base_representation, ReturnType>::value), 
-                        "return type is expected to be a representation class");
-
-                    return ReturnType(data);
-                }
-
-                //!angular separation between two coordinates in radians
-                template <typename OtherCoordinate>
-                double separation(OtherCoordinate const& other) const
-                {
-                    BOOST_STATIC_ASSERT_MSG((boost::astronomy::detail::is_base_template_of
-                        <boost::astronomy::coordinate::base_frame, OtherCoordinate>::value),
-                        "argument type is expected to be a coordinate class");
-
-                    return std::acos(this->data.dot(other.get_data()));
-                }
-
+    //!angular separation between two coordinates in radians
+    bu::quantity<bu::si::dimensionless> separation(base_frame const& other) const
+    {
+        return bu::quantity<bu::si::dimensionless>::from_value(
+            std::acos(dot(this->data, other.get_data()))
+        );
+    }
                 
-            };
-        } //namespace coordinate
-    } //namespace astronomy
-} //namespace boost
+};
+}}} //namespace boost::astronomy::coordinate
 #endif  // !BOOST_ASTRONOMY_COORDINATE_BASE_COORDINATE_SYSTEM_HPP
