a8f4d5dfadcfb820ef1dd6784b216e670d895b68
Pranam Lashkari <plashkari628@gmail.com>
2019-07-30 19:41:20 +0530

    `is_base_frame_of` struct added (#63)
    This struct will allow to check if template types are derived from frame
    
    closes #61

--- include/boost/astronomy/detail/is_base_template_of.hpp
+++ include/boost/astronomy/detail/is_base_template_of.hpp
@@ -5,33 +5,45 @@
 #include <type_traits>
 
 
-namespace boost
+namespace boost { namespace astronomy { namespace detail {
+
+///@cond INTERNAL
+// structure to provide support like std::is_base_of for template base classes
+template <template <std::size_t, typename...> class Base, typename Derived>
+struct base_template
 {
-    namespace astronomy
-    {
-        namespace detail
-        {
-            ///@cond INTERNAL
-            // structure to provide support like std::is_base_of for template base classes
-            template <template <std::size_t, typename...> class Base, typename Derived>
-            struct base_template
-            {
-                //using U = typename std::remove_cv<Derived>::type;
+    //using U = typename std::remove_cv<Derived>::type;
 
-                template <std::size_t T, typename... Args>
-                static std::true_type test(const Base<T, Args...>*);
+    template <std::size_t T, typename... Args>
+    static constexpr std::true_type test(const Base<T, Args...>*);
 
-                static std::false_type test(...);
+    static constexpr std::false_type test(...);
 
-                using type = decltype(test(std::declval<Derived*>()));
-            };
+    using type = decltype(test(std::declval<Derived*>()));
+};
 
-            template <template <std::size_t, typename...> class Base, typename Derived>
-            using is_base_template_of = typename base_template<Base, Derived>::type;
-            ///@endcond
-        } //namespace detail
-    } //namespace astronomy
-} //namespace boost
+template <template <std::size_t, typename...> class Base, typename Derived>
+using is_base_template_of = typename base_template<Base, Derived>::type;
+
+
+// structure to provide support like std::is_base_of for template base classes
+template <template <typename...> class Base, typename Derived>
+struct base_frame_template
+{
+    template <typename... Args>
+    static constexpr std::true_type test(const Base<Args...> *);
+
+    static constexpr std::false_type test(...);
+
+    using type = decltype(test(std::declval<Derived*>()));
+};
+
+template <template <typename...> class Base, typename Derived>
+using is_base_frame_of = typename base_frame_template<Base, Derived>::type;
+
+///@endcond
+
+}}} //namespace boost::astronomy::detail
 
 #endif // !BOOST_ASTRONOMY_DETAIL_IS_BASE_TEMPLATE_OF_HPP
 
